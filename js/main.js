9a48ac8231086506c2b09353c909c442797b0bce
Antonio Campos <antonio.hdez93@gmail.com>
2018-12-26 21:56:00 -0600

    Proof of concept.

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const tweetsContainer = document.querySelector( '#stream-items-id' );
+const tweets = tweetsContainer.querySelectorAll( '[data-item-type="tweet"]' );
+
+function filterTweets( tweets ) {
+    Array.from( tweets ).forEach(( tweet ) => {
+        const dataNode = tweet.querySelector( '[data-you-follow]' );
+    
+        if ( dataNode ) {
+            const following = JSON.parse( dataNode.dataset.youFollow ); // 'JSON.parse' checks for valid boolean values...
+            const retweet = dataNode.dataset.retweetId; // Could be 'undefined'
+            const self = dataNode.classList.contains( 'my-tweet' ) // My own tweets.
+    
+            if ( !following && !retweet && !self ) {
+                    tweet.style.opacity = 0.25;
+            }
+    
+        } else { // Quoted tweet
+            // tweet.style.opacity = 0.25;
+        }
+    });
+}
+
+var observer = new MutationObserver(function(mutations, observer) {
+    mutations.forEach(( mutation ) => {
+        if ( mutation.type === 'childList' && mutation.addedNodes.length > 0 )  {
+            const tweets = Array.from( mutation.addedNodes ).filter(( node ) => 
+                node.tagName.toLowerCase() === 'li' &&
+                node.dataset.itemType === 'tweet'
+            );
+
+            filterTweets( tweets );
+        }
+    })
+});
+
+observer.observe(tweetsContainer, { childList: true });
+
+filterTweets( tweets );
